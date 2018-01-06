30cfd3a1c84224eb9723b031ddf4cf8796e4932d
SAPIENT\kmridu <kmridul@sapient.com>
2018-01-06 13:48:36 +0530

    added files

--- reactquiz/src/App.js
+++ reactquiz/src/App.js
@@ -3,7 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {quizData :''}
+	}
+	
   render() {
+	  //showQuiz(this.state.quizData);
+	  var a = this.state.quizData;
+	  debugger;
     return (
       <div className="App">
         <header className="App-header">
@@ -16,6 +24,25 @@ class App extends Component {
       </div>
     );
   }
+     /*const showQuiz = (quizData) => {
+		if(quizData !== ''){
+			debugger;
+		}
+	}*/
+  componentDidMount() {
+    //   fetch('http://localhost:3000/quiz',{
+		// method:'GET'
+		// }
+		// ).then((response) => {
+    //   debugger;
+		// 	return response.json().then(json => {
+		// 		this.setState({
+    //             quizData: json
+    //         });
+		// 	});
+		// });
+    }
+	
 }
 
 export default App;
