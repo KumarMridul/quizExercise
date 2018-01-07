dd5867fd7f6ffdbd7bdfb7327636c6499db30786
SAPIENT\kmridu <kmridul@sapient.com>
2018-01-07 20:57:46 +0530

    webpack error fix

--- src/reducers/updateAnswers.js
+++ src/reducers/updateAnswers.js
@@ -1,11 +1,11 @@
 export function updateAnswers (state = [], action) {
     switch ( action.type ) {
         case 'SELECTED_ANSWER':
+
             let isAvailable = false;
-            debugger;
             state.forEach( (eachAnswer, index) => {
                 if ( eachAnswer.questionName === action.answer.questionName ) {
-                    state[index] = action.answer;
+                    state[index] = action.answer;
                     isAvailable = true;
                 }
             });
